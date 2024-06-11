3230ee906fce1f3f34845545a652ecb34ed9b3fb
Godwill Kiplagat <godwillkiprop@gmail.com>
2024-06-11 18:35:47 +0300

    Python practice

--- 4_python_loops.py
+++ 4_python_loops.py
@@ -1,8 +1,20 @@
 
 
 # Create a list of possible values
-values = [2, 3, 4, "apple", "banana", "cat", "dog"]
+# values = [2, 3, 4, "apple", "banana", "cat", "dog"]
 
-for i in values:
-    print(i)
+# Days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+# for i in values:
+#     for v in Days:
+#         print(i,v)
+
+count = 0
+
+while (count <= 10):
+    print (f"Counter is currently at {count}")
+    if (count%2 == 0):
+        print(f"{count} is even")
+    else:
+        print(f"{count} is odd")
+    count += 1
 
